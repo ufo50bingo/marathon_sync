f8a64cfd622cb44f46c2551c49bccfec3e8ceeb8
Frank Thompson <frankthompson@gmail.com>
2026-08-09 16:55:21 -0700

    basic api calls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+pub fn main() {
+    // totals
+    // let url = "https://donate.cherry-rush.org/tracker/api/v2/events/2/?totals";
+    // incentives
+    let url = "https://donate.cherry-rush.org/tracker/api/v2/bids/";
+    let result = reqwest::blocking::get(url)
+        .expect("request failed")
+        .text()
+        .expect("body failed");
+
+    println!("{}", result);
 }
